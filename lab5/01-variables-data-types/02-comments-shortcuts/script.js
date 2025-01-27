06b8474610d1863d592ffe7c16844f8316f3c901
“Kittaphon-la” <kittaphon_la67@live.rmutl.ac.th>
2025-01-27 12:05:51 +0700

    lab5

---
+++ lab5/01-variables-data-types/02-comments-shortcuts/script.js
@@ -0,0 +1,9 @@
+// this is a single line of code
+
+// multi-line comment
+console.log("D ka");
+
+/*
+this is a multi-line comment
+and it spans multiple lines
+*/
---
+++ lab5/01-variables-data-types/03-variables/script.js
@@ -0,0 +1,8 @@
+console.log("Varibles");
+
+let fisrtName = 'John';
+const lastName = 'Doe';
+let age = 30;
+
+console.log(fisrtName+lastName);
+console.log(fisrtName);
